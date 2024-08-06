b64b3f1d25f30f020dae6829be8c911d4c195c0f
grushaway <gloom_lie@bk.ru>
2024-08-06 16:36:36 +0200

    js update

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
+function updateListeners() {
   var backToTopButton = document.getElementById("back-to-top");
   var header = document.getElementById("header");
 
   window.addEventListener("scroll", function () {
+    console.log("scrolling");
     const firstContainer = document.getElementById("first-container");
     const firstContainerBottom = firstContainer.getBoundingClientRect().bottom;
 
-    if (firstContainerBottom < 0) {
-      backToTopButton.style.display = "block";
-    } else {
-      backToTopButton.style.display = "none";
-    }
-  });
-
-  window.addEventListener("scroll", function () {
     var header = document.getElementById("navbar");
     var spacer = document.querySelector(".spacer");
     var spacerBottom = spacer.offsetTop + spacer.offsetHeight;
@@ -23,6 +16,12 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       header.classList.remove("fixed");
     }
+
+    if (firstContainerBottom < 0) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
   });
 
   backToTopButton.addEventListener("click", function () {
@@ -31,48 +30,65 @@ document.addEventListener("DOMContentLoaded", function () {
       behavior: "smooth",
     });
   });
+}
 
-  function loadContent() {
-    const themeStylesheet = document.getElementById("theme-stylesheet");
-    const content = document.getElementById("content-table");
-    const userAgent = navigator.userAgent.toLowerCase();
-    const isMobile =
-      /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(
-        userAgent
-      );
+function loadContent() {
+  updateListeners();
+  const themeStylesheet = document.getElementById("theme-stylesheet");
+  const content = document.getElementById("content-table");
+  const userAgent = navigator.userAgent.toLowerCase();
+  const isMobile =
+    /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(
+      userAgent
+    );
 
-    if (isMobile) {
-      themeStylesheet.setAttribute("href", "mobile.css");
-      fetch("mobile.html")
-        .then((response) => response.text())
-        .then((data) => {
-          content.innerHTML = data;
-        });
-    } else {
-      themeStylesheet.setAttribute("href", "stylemain.css");
-      fetch("index.html")
-        .then((response) => response.text())
-        .then((data) => {
-          content.innerHTML = data;
-        });
-    }
+  // if (isMobile) {
+  //   themeStylesheet.setAttribute("href", "mobile.css");
+  //   fetch("mobile.html")
+  //     .then((response) => response.text())
+  //     .then((data) => {
+  //       content.innerHTML = data;
+  //     });
+  //   return;
+  // }
 
-    function switchLanguage(lang) {
-      const texts = document.querySelectorAll(".text-lang");
-      texts.forEach((text) => {
-        if (text.id.includes(lang)) {
-          text.style.display = "block";
-        } else {
-          text.style.display = "none";
-        }
-      });
-    }
-    
-    loadContent();
-
-    window.addEventListener("resize", loadContent);
-    window.addEventListener("load", loadContent);
-    document.getElementById("current-language").textContent =
-      lang.toUpperCase();
+  var fileName;
+  switch (lang) {
+    case "en":
+      fileName = isMobile ? "mobile.html" : "index.html";
+      break;
+    case "ru":
+      fileName = isMobile ? "mobile_ru.html" : "flat_ru.html";
+      break;
+    case "sr":
+      fileName = isMobile ? "mobile_sr.html" : "flat_sr.html";
+      break;
+    default:
+      fileName = isMobile ? "mobile.html" : "index.html";
   }
-});
+  themeStylesheet.setAttribute(
+    "href",
+    isMobile ? "mobile.css" : "stylemain.css"
+  );
+  fetch(fileName)
+    .then((response) => response.text())
+    .then((data) => {
+      content.innerHTML = data;
+    });
+}
+
+function switchLanguage(lang) {
+  const texts = document.querySelectorAll(".text-lang");
+  texts.forEach((text) => {
+    if (text.id.includes(lang)) {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  });
+}
+// document.getElementById("current-language").textContent = lang.toUpperCase();
+
+document.addEventListener("DOMContentLoaded", loadContent);
+window.addEventListener("resize", loadContent);
+window.addEventListener("load", loadContent);
